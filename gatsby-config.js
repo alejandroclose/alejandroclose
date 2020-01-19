865409369004466a86159e88301f8d264ba13744
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2020-01-19 16:09:06 +0100

    added google analytics and updated gatsby version

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/panti.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-7383220-7",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
